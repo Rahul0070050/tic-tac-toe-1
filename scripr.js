8d41e3b6c77207972d4147aa04e805a5c1d33ca4
rahul <rahulor2000@gmail.com>
2022-03-07 17:52:42 +0530

    adding btn function
    almost finished

---
+++ scripr.js
@@ -0,0 +1,88 @@
+const bord = document.getElementById("bord");
+const col1 = document.getElementById("1");
+const col2 = document.getElementById("2");
+const col3 = document.getElementById("3");
+const col4 = document.getElementById("4");
+const col5 = document.getElementById("5");
+const col6 = document.getElementById("6");
+const col7 = document.getElementById("7");
+const col8 = document.getElementById("8");
+const col9 = document.getElementById("9");
+
+var icon = "x"
+
+document.addEventListener("click", (e) => {
+    var id = e.target.id;
+    if (id >= 1 && id <= 9) {
+        if (icon == "x") {
+            icon = "o"
+        } else {
+            icon = "x"
+        }
+        var col = document.getElementById(id);
+        if (!col.innerHTML) {
+            col.innerHTML = icon
+            if (id == "1") {
+                if (col2.innerHTML == icon && col3.innerHTML == icon ||
+                    col4.innerHTML == icon && col7.innerHTML == icon ||
+                    col5.innerHTML == icon && col9.innerHTML == icon) {
+                    alert(`${icon} wons`)
+                }
+            } else if (id == "2") {
+                if (col1.innerHTML == icon && col3.innerHTML == icon ||
+                    col5.innerHTML == icon && col8.innerHTML == icon) {
+                    alert(`${icon} wons`)
+                }
+            } else if (id == "3") {
+                if (col1.innerHTML == icon && col2.innerHTML == icon ||
+                    col6.innerHTML == icon && col9.innerHTML == icon ||
+                    col5.innerHTML == icon && col7.innerHTML == icon) {
+                    alert(`${icon} wons`)
+                }
+            } else if (id == "4") {
+                if (col1.innerHTML == icon && col7.innerHTML == icon ||
+                    col5.innerHTML == icon && col6.innerHTML == icon) {
+                    alert(`${icon} wons`)
+                }
+            } else if (id == "5") {
+                if (col1.innerHTML == icon && col9.innerHTML == icon ||
+                    col3.innerHTML == icon && col7.innerHTML == icon ||
+                    col2.innerHTML == icon && col8.innerHTML == icon ||
+                    col4.innerHTML == icon && col6.innerHTML == icon)
+                    alert(`${icon} wons`)
+            } else if (id == "6") {
+                if (col3.innerHTML == icon && col9.innerHTML == icon ||
+                    col5.innerHTML == icon && col4.innerHTML == icon) {
+                    alert(`${icon} wons`)
+                }
+            } else if (id == "7") {
+                if (col1.innerHTML == icon && col4.innerHTML == icon ||
+                    col8.innerHTML == icon && col9.innerHTML == icon ||
+                    col5.innerHTML == icon && col3.innerHTML == icon) {
+                    alert(`${icon} wons`)
+                }
+            } else if (id == "8") {
+                if (col9.innerHTML == icon && col7.innerHTML == icon ||
+                    col5.innerHTML == icon && col2.innerHTML == icon) {
+                    alert(`${icon} wons`)
+                }
+            } else if (id == "9") {
+                if (col6.innerHTML == icon && col3.innerHTML == icon ||
+                    col7.innerHTML == icon && col8.innerHTML == icon ||
+                    col5.innerHTML == icon && col1.innerHTML == icon) {
+                    alert(`${icon} wons`)
+                }
+            }
+        }
+    } else {
+        return
+    }
+})
+
+function reset() {
+    alert()
+    for (let i = 1; i <= 9; i++) {
+        var col = document.getElementById(i);
+        col.innerHTML = "";
+    }
+}
